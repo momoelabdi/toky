83be9e8eca71708ddab0b57d52c478c9a864d30b
momoelabdi <momo.elabdi@bluewin.ch>
2023-02-20 15:22:26 +0100

    seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-50.times do |i|
-    Client.create(
-        name: "Hanks #{i + 1}",
-        last_name: "Doe ",
-        addres: "smaty 123",
-        date_of_birth: "2002-02-02",
-        comment: "Comment of the client some text here and there",
-        phone:  123456789,
-        email: "email@gmail.com"
-    )
-end 
+# 50.times do |i|
+#     Client.create(
+#         name: "Hanks #{i + 1}",
+#         last_name: "Doe ",
+#         addres: "smaty 123",
+#         date_of_birth: "2002-02-02",
+#         comment: "Comment of the client some text here and there",
+#         phone:  123456789,
+#         email: "email@gmail.com"
+#     )
+# end 
